4088377a34c1d0d08ce799e2d33461fbd686e7a0
Taras Zhyhulin <zhyhulin@student.agh.edu.pl>
2024-01-07 21:44:39 +0100

    added update translation possibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlparse
-from sql_dictionary import translations
+from sql_dictionary import translations as trs
 
 def translate_sql_query(sql_query):
     parsed = sqlparse.parse(sql_query)[0]
@@ -7,28 +7,46 @@ def translate_sql_query(sql_query):
 
     translation = "This query will "
     columns = []
+    values = []
     table_name = ""
     order_by_column = ""
     order_direction = "ascending"  # Default ordering
+    bracket = False
+    set_keyword = False
 
     for token in tokens:
         if token.ttype is sqlparse.tokens.DML and token.value.upper() == 'SELECT':
             columns = []  # Reset columns list for each SELECT statement
+            values = []
             translation += "select "
 
         elif token.ttype is sqlparse.tokens.Wildcard:
             columns = ["all columns"]  # If wildcard is found, we want all columns
 
-        elif token.ttype is sqlparse.tokens.Name:
-            if 'from' in translation and 'select' in translation and not table_name:  # Capture the table name
+        if token.ttype is sqlparse.tokens.Name:
+            if ('from' in translation and 'select' in translation) or 'update' in translation and not table_name:  # Capture the table name
                 table_name = token.value
             elif 'select' in translation:  # Otherwise, it's likely a column name
                 columns.append(token.value)
+
+            if 'insert into' in translation and not table_name and not bracket:
+                table_name = token.value
+            elif 'insert into' in translation and bracket:
+                columns.append(token.value)
+            elif 'update' in translation and set_keyword:
+                columns.append(token.value)
+
+        if ("Token.Literal.String" in str(token.ttype)) or ("Token.Literal.Number" in str(token.ttype)):
+            values.append(token.value)
 
         elif token.ttype is sqlparse.tokens.Keyword:
             keyword = token.value.upper()
             if keyword == 'FROM':
                 translation += ', '.join(columns) + " from "  # Append columns and FROM clause
+            if keyword == 'INTO':
+                translation += trs[keyword]
+            if keyword == 'SET':
+                set_keyword = True
 
             elif keyword == 'ORDER BY':
                 order_by_column = tokens[tokens.index(token) + 2].value  # Assuming next significant token is the column name
@@ -39,6 +57,14 @@ def translate_sql_query(sql_query):
     # Append table name, order by column and direction to translation
     if table_name:
         translation += table_name + " table"
+    if table_name in translation and set_keyword:
+        translation += " by setting "
+    if not bracket and values and columns:
+        for i in range(len(columns)):
+            translation += columns[i] + " value " + values[i]
+            if i != len(columns) - 1:
+                translation += ", "
+
     if order_by_column:
         translation += f" ordered by {order_by_column} {order_direction}"
 
